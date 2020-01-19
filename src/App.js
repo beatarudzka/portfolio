ef58037193e1d2919513e8ee6f3c75a26b26a7b5
Beata Rudzka <beatarudzka89@gmail.com>
2020-01-19 11:43:09 +0100

    configuring gulp, bootstrap-react and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React from 'react'
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      Header
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    title: 'Beata Rudzka',
+    headerLinks: [
+      { title: 'Home', path: '/' },
+      { title: 'Projects', path: '/Projects' },
+      { title: 'Contact', path: '/Contact' }
+    ],
+    home: {
+      title: 'Cześć, jestem Beata',
+      subtitle: 'Web developer',
+      text: 'Więcej o mnie'
+    },
+    projects: {
+      title: 'Moje projekty',
+    },
+    contact: {
+      title: 'Porozmawiajmy',
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Container fluid={true}>
+          <Navbar className="border-bottom">
+            <Navbar.Brand>Beata Rudzka</Navbar.Brand>
+          </Navbar>
+        </Container>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
